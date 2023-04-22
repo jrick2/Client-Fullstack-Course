6efc1ddfc4942818f920c28334c1b0f59c45e84b
jrick2 <joshrick16.abellera@gmail.com>
2023-04-22 01:10:53 -0500

    bug fixed

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -62,7 +62,7 @@ const UserProvider = ({ children }: any) => {
         error: null,
       });
     }
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <UserContext.Provider value={[user, setUser]}>
